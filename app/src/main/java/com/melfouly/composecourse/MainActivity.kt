cc0ef0df26c3da01492ea83f1aad4548ccfa7e55
Mahmoud Reda <mahmoudelfoulyyy@gmail.com>
2023-07-18 15:52:40 +0300

    Initial commit

--- app/src/main/java/com/melfouly/composecourse/MainActivity.kt
+++ app/src/main/java/com/melfouly/composecourse/MainActivity.kt
@@ -4,38 +4,14 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxScope
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Badge
-import androidx.compose.material.BadgedBox
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import com.melfouly.composecourse.ui.BottomNavItem
-import com.melfouly.composecourse.ui.Screen
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.rememberCoroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -44,131 +20,51 @@ class MainActivity : ComponentActivity() {
 
         setContent {
 
-            val navController = rememberNavController()
-            Scaffold(bottomBar = {
-                BottomNavigationBar(
-                    items = listOf(
-                        BottomNavItem(
-                            name = "Home",
-                            route = Screen.Home.route,
-                            icon = Icons.Default.Home
+            val scaffoldState = rememberScaffoldState()
+            val scope = rememberCoroutineScope()
+            Scaffold(
+                scaffoldState = scaffoldState,
+                topBar = {
+                    AppBar(
+                        onNavigationIconClick = {
+                            scope.launch {
+                                scaffoldState.drawerState.open()
+                            }
+                        }
+                    )
+                },
+                drawerContent = {
+                    DrawerHeader()
+                    DrawerBody(
+                        items = listOf(
+                            MenuItem(
+                                id = "home",
+                                title = "Home",
+                                icon = Icons.Default.Home
+                            ),
+                            MenuItem(
+                                id = "settings",
+                                title = "Settings",
+                                icon = Icons.Default.Settings
+                            ),
+                            MenuItem(
+                                id = "help",
+                                title = "Help",
+                                icon = Icons.Default.Info
+                            )
                         ),
-                        BottomNavItem(
-                            name = "Chat",
-                            route = Screen.Notification.route,
-                            icon = Icons.Default.Notifications,
-                            badgeCount = 13
+                        onItemClick = {
+                            println("Clicked on ${it.title}")
+                        }
+                    )
+                },
+                drawerGesturesEnabled = scaffoldState.drawerState.isOpen
+            ) {
 
-                        ),
-                        BottomNavItem(
-                            name = "Settings",
-                            route = Screen.Settings.route,
-                            icon = Icons.Default.Settings
-                        )
-                    ),
-                    navController = navController,
-                )
-            }) {
-                Navigation(navController = navController)
             }
 
         }
     }
 }
 
-@Composable
-fun Navigation(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = Screen.Home.route) {
-        composable(Screen.Home.route) {
-            HomeScreen()
-        }
-        composable(Screen.Notification.route) {
-            NotificationScreen()
-        }
-        composable(Screen.Settings.route) {
-            SettingsScreen()
-        }
-    }
-}
 
-@Composable
-fun BottomNavigationBar(
-    items: List<BottomNavItem>,
-    navController: NavController,
-    modifier: Modifier = Modifier,
-) {
-    val backStackEntry = navController.currentBackStackEntryAsState()
-    BottomNavigation(
-        modifier = modifier,
-        backgroundColor = Color.DarkGray,
-        elevation = 5.dp
-    ) {
-        items.forEach { item ->
-            val selected = item.route == backStackEntry.value?.destination?.route
-
-            BottomNavigationItem(selected = selected,
-                onClick = {
-                    navController.navigate(item.route) {
-                        popUpTo(navController.graph.startDestinationId)
-                        launchSingleTop = true
-                    }
-                },
-                selectedContentColor = Color.Green,
-                unselectedContentColor = Color.Gray,
-                icon = {
-                    Column(horizontalAlignment = CenterHorizontally) {
-                        if (item.badgeCount > 0) {
-                            BadgedBox(badge = {
-                                Badge {
-                                    Text(text = item.badgeCount.toString(), color = Color.White)
-                                }
-                            }) {
-                                Icon(imageVector = item.icon, contentDescription = item.name)
-                            }
-                        } else {
-                            Icon(imageVector = item.icon, contentDescription = item.name)
-                        }
-                        if (selected) {
-                            Text(
-                                text = item.name,
-                                textAlign = TextAlign.Center,
-                                fontSize = 16.sp,
-                                color = Color.White
-                            )
-                        }
-                    }
-                }
-            )
-        }
-    }
-}
-
-@Composable
-fun HomeScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Home Screen")
-    }
-}
-
-@Composable
-fun NotificationScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Chat Screen")
-    }
-}
-
-@Composable
-fun SettingsScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Settings Screen")
-    }
-}
--- app/src/main/java/com/melfouly/composecourse/ui/Screen.kt
+++
@@ -1,7 +0,0 @@
-package com.melfouly.composecourse.ui
-
-sealed class Screen(val route: String) {
-    object Home: Screen("home_screen")
-    object Notification: Screen("notification_screen")
-    object Settings: Screen("settings_screen")
-}
